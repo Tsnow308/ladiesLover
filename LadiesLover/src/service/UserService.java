bf07d28a6ddfd800db4e800a370b46af0d43a922
chenyufei <chenyufei_fly@163.com>
2015-06-30 18:03:10 +0800

    解决了注册不写入数据库的问题

--- LadiesLover/src/service/UserService.java
+++ LadiesLover/src/service/UserService.java
@@ -2,8 +2,19 @@ package service;
 
 import java.util.List;
 
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
+
 import model.User;
 import model.UserDAO;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.opensymphony.xwork2.config.Configuration;
 
 public class UserService {
 	
@@ -37,11 +48,27 @@ public class UserService {
 			 return false;
 		 }
 		 else{
+			 org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration().configure();
+			 SessionFactory sessionFactory = configuration.buildSessionFactory();
+			 Session session = sessionFactory.openSession();
+			 session.beginTransaction();
+			 
 			 User newuser = new User();
 			 newuser.setUsername(username);
 			 newuser.setPassword(password);
 			 newuser.setEmail(username);
-			 userDAO.save(newuser);
+			 //userDAO.save(newuser);
+			 session.save(newuser);
+			 
+			 session.getTransaction().commit();
+			 
+//			 try {
+//				transaction.commit();
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			} 
+			 
 			 return true;
 		 }
 	}
